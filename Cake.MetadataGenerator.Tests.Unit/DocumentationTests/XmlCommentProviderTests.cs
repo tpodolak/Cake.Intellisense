85f852ae2a05d96ff347cd18e322158f5fb272d8
tpodolak <misiek767@gmail.com>
2017-02-06 00:32:45 +0100

    Scaffolding for XmlCommentProvider tests

---
+++ Cake.MetadataGenerator.Tests.Unit/DocumentationTests/XmlCommentProviderTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+using Cake.MetadataGenerator.Documentation;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using NSubstitute;
+using Xunit;
+
+namespace Cake.MetadataGenerator.Tests.Unit.DocumentationTests
+{
+    public class XmlCommentProviderTests : Test<XmlCommentProvider>
+    {
+        private readonly XDocument emptyDocument = XDocument.Parse("<?xml version=\"1.0\"?><doc></doc>");
+
+        [Fact]
+        public void GetReturnsEmptyStringWhenSectionForGivenCommentIdDoesNotExist()
+        {
+            var symbol = Substitute.For<ISymbol>();
+            symbol.GetDocumentationCommentId().Returns(string.Empty);
+
+            var result = Subject.Get(emptyDocument, symbol);
+
+            result.Should().BeEmpty();
+        }
+    }
+}
