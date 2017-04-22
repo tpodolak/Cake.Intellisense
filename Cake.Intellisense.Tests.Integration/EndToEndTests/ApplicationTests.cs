9fe50f68504ebf90e957a5fe30b5a31746cd5329
tpodolak <misiek767@gmail.com>
2017-04-22 21:25:02 +0200

    [GH-17] - Running End-To-End tests in separate AppDomains

--- Cake.Intellisense.Tests.Integration/EndToEndTests/ApplicationTests.cs
+++ Cake.Intellisense.Tests.Integration/EndToEndTests/ApplicationTests.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Cake.Intellisense.CodeGeneration.MetadataGenerators;
-using Cake.Intellisense.Tests.Integration.Extensions;
+using AppDomainToolkit;
 using Xunit;
 
 namespace Cake.Intellisense.Tests.Integration.EndToEndTests
@@ -13,77 +11,53 @@ namespace Cake.Intellisense.Tests.Integration.EndToEndTests
         public class RunMethod
         {
             private const string DefaultFramework = ".NETFramework,Version=v4.5";
-            private readonly Application _application = new Application();
 
             public static IEnumerable<object[]> CakePackages
             {
                 get
                 {
-                    yield return new object[] { "Cake.Common", DefaultFramework, "0.19.1" };
-
-                    // TODO Get rid of loading assembly to AppDomain
-                    // yield return new object[] { "Cake.Powershell", DefaultFramework, "0.3.0" };
+                    yield return new object[] { CreateMetadataGeneratorOptions("Cake.Common", DefaultFramework, "0.19.1") };
+                    yield return new object[] { CreateMetadataGeneratorOptions("Cake.Powershell", DefaultFramework, "0.3.0") };
+                    yield return new object[] { CreateMetadataGeneratorOptions("Cake.Coveralls", DefaultFramework, "0.4.0") };
                 }
             }
 
             public static IEnumerable<object[]> CakeCorePackages
             {
-                get
-                {
-                    yield return new object[] { DefaultFramework, "0.19.1" };
-                }
+                get { yield return new object[] { CreateMetadataGeneratorOptions("Cake.Core", DefaultFramework, "0.19.1") }; }
             }
 
             [Theory]
             [MemberData(nameof(CakePackages))]
-            public void CanGenerateMetadataForPackageTest(string package, string framework, string version)
+            public void CanGenerateMetadataForPackageTest(string[] options)
             {
-                var options = CreateMetadataGeneratorOptions(package, framework, version);
-
-                var result = _application.Run(options);
-
-                PrepareAssemblies(result);
-                result.Should().NotBeNull().And.GenerateValidCakeAssemblies();
+                IsolateRun(proxy => proxy.VerifyCakePackage(options));
             }
 
             [Theory]
             [MemberData(nameof(CakeCorePackages))]
-            public void CanGenerateMetadataForCakeCoreLibTest(string framework, string version)
+            public void CanGenerateMetadataForCakeCoreLibTest(string[] options)
             {
-                var options = CreateMetadataGeneratorOptions("Cake.Core", framework, version);
-
-                var result = _application.Run(options);
-
-                PrepareAssemblies(result);
-                result.Should().NotBeNull().And.GenerateValidCakeCoreAssemblies();
+                IsolateRun(proxy => proxy.VerifyCakeCorePackage(options));
             }
 
-            private string[] CreateMetadataGeneratorOptions(string package, string targetFramework, string version)
+            private void IsolateRun(Action<TestCaseProxy> action)
+            {
+                var context = AppDomainContext.Create(AppDomain.CurrentDomain.SetupInformation);
+                using (var remoteGreeter = Remote<TestCaseProxy>.CreateProxy(context.Domain))
+                {
+                    action(remoteGreeter.RemoteObject);
+                }
+            }
+
+            private static string[] CreateMetadataGeneratorOptions(string package, string targetFramework, string version)
             {
                 return new[]
                 {
-                    "--Package", package, "--PackageVersion", version ?? string.Empty, "--TargetFramework", targetFramework,
+                    "--Package", package, "--PackageVersion", version ?? string.Empty, "--TargetFramework",
+                    targetFramework,
                     "--OutputFolder", Path.Combine(Environment.CurrentDirectory, "Result")
                 };
-            }
-
-            private void PrepareAssemblies(GeneratorResult result)
-            {
-                var referencedAssemblies = result?.EmitedAssemblies
-                                               .SelectMany(assembly => assembly.GetReferencedAssemblies()
-                                                   .Select(val => val.FullName))
-                                               .ToList() ?? new List<string>();
-
-                // TODO get rid of manual coping and assembly loading
-                var locations = AppDomain.CurrentDomain
-                                         .GetAssemblies()
-                                         .Where(assembly => referencedAssemblies.Contains(assembly.FullName))
-                                         .Select(assembly => assembly.Location);
-
-                foreach (var val in locations)
-                {
-                    File.Copy(val, Path.Combine(Environment.CurrentDirectory, Path.GetFileName(val)), true);
-                }
             }
         }
     }
---
+++ Cake.Intellisense.Tests.Integration/Exceptions/SerializableAssertionException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Cake.Intellisense.Tests.Integration.Exceptions
+{
+    [Serializable]
+    public class SerializableAssertionException : Exception
+    {
+        public SerializableAssertionException()
+        {
+        }
+
+        public SerializableAssertionException(string message)
+            : base(message)
+        {
+        }
+
+        public SerializableAssertionException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected SerializableAssertionException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
